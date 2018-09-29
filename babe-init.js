c1ed058c68959dbc9e43f2dbca1f980163c01006
Stela <stella.plamenova@gmail.com>
2018-09-29 14:07:59 +0200

    prints info about the experiment in the browser's console

--- babe-init.js
+++ babe-init.js
@@ -32,6 +32,60 @@ function babeInit(config) {
     _babe.deploy.is_MTurk = _babe.deploy.MTurk_server !== "";
     _babe.deploy.submissionURL = _babe.deploy.deployMethod == "localServer" ? "http://localhost:4000/api/submit_experiment/" + _babe.deploy.experimentID : _babe.deploy.serverAppURL + _babe.deploy.experimentID;
 
+    if (_babe.deploy.deployMethod === 'MTurk' || _babe.deploy.deployMethod === 'MTurkSandbox') {
+        console.log(
+`The experiment runs on MTurk (or MTurk's sandbox)
+----------------------------
+
+The ID of your experiment is ${_babe.deploy.experimentID}
+
+The results will be submitted ${_babe.deploy.submissionURL}
+
+and
+
+MTurk's server: ${_babe.deploy.MTurk_server}`
+);
+    } else if (_babe.deploy.deployMethod === 'Prolific') {
+        console.log(
+`The experiment runs on Prolific
+-------------------------------
+
+The ID of your experiment is ${_babe.deploy.experimentID}
+
+The results will be submitted to ${_babe.deploy.submissionURL}
+
+with
+
+Prolific URL (must be the same as in the website): ${_babe.deploy.prolificURL}`
+);
+    } else if (_babe.deploy.deployMethod === 'directLink') {
+        console.log(
+`The experiment uses Direct Link
+-------------------------------
+
+The ID of your experiment is ${_babe.deploy.experimentID}
+
+The results will be submitted to ${_babe.deploy.submissionURL}`
+);
+    } else if (_babe.deploy.deployMethod === 'debug') {
+        console.log(
+`The experiment is in Debug Mode
+-------------------------------
+
+The results will be displayed in a table at the end of the experiment and available to download in CSV format.`
+);
+    } else {
+
+        throw new Error(
+`There is no such deployMethod.
+
+Please use 'debug', 'directLink', 'Mturk', 'MTurkSandbox' or 'Prolific'.
+
+The deploy method you provided is '${_babe.deploy.deployMethod}'.
+
+You can find more information at https://github.com/babe-project/babe-base`);
+    }
+
     // adds progress bars to the views
     addProgressBars();
 
